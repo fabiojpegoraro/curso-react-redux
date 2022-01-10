434b9588724c3ac6a86aaaf8f5036486e9ffc82b
Fábio José Pegoraro <fabio.pegoraro@softplan.com.br>
2022-01-10 19:47:14 -0300

    Fundamentos React

--- fundamentos-react/src/components/basicos/Aleatorio.jsx
+++ fundamentos-react/src/components/basicos/Aleatorio.jsx
@@ -4,10 +4,18 @@ const aleatorio = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-export default function Aleatorio(props) {
+export default (props) => {
     return (    
         <div>
-            O número é {aleatorio(props.min, props.max)}
+            <p>
+                <strong>Valor Mínimo é: {props.min}</strong>
+            </p>
+            <p>
+                <strong>Valor Máximo é: {props.max}</strong>
+            </p>
+            <p>
+            <strong>Valor Escolhido é: {aleatorio(props.min, props.max)}</strong>
+            </p>
         </div>
     );
 };
