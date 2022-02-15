8079c04a33841f3a906e3ccb837e6125a10a752c
Fábio José Pegoraro <fabio.pegoraro@softplan.com.br>
2022-02-15 08:06:37 -0300

    hooks

--- hooks/src/views/examples/UseCallback.jsx
+++ hooks/src/views/examples/UseCallback.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useCallback, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import UseCallbackButtons from './UseCallBackButtons';
 
 const UseCallback = (props) => {
+
+    const[count, setCount] = useState(0);
+
+    // const inc = (delta) => {
+    //     setCount(count => count + delta);
+    // }
+
+    const inc = useCallback((delta) => {
+        setCount(current => current + delta);
+    }, [setCount])
+
     return (
         <div className="UseCallback">
             <PageTitle
                 title="Hook UseCallback"
                 subtitle="Retorna uma função memoizada!"
             />
+            <div className="center">
+                <span className="text">{count}</span>
+                <UseCallbackButtons inc={inc}/>
+            </div>
         </div>
     )
 }
--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,13 +1,78 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+
+
+const merge = (s1, s2) => {
+    // const arrays1 = Array.from(s1);
+    // const arrays2 = Array.from(s2);
+    // const sizeArrays = arrays1.length + arrays2.length;
+
+    // const resultado = [];
+
+    // for(let i = 0; i < sizeArrays; i++){
+    //     resultado.push(arrays1[i]);
+    //     resultado.push(arrays2[i]);
+    //     }
+
+    // return resultado;
+
+    return [...s1].map((w, i) => `${w}${s2[i] || ""}`);
+}
 
 const UseRef = (props) => {
+    const [value1, setValue1] = useState('');
+    const [value2, setValue2] = useState('');
+
+    const count = useRef(0);
+    const input1 = useRef(null);
+    const input2 = useRef(null);
+
+    useEffect(() => {
+        count.current++;
+        input2.current.focus();
+    },[value1])
+
+    useEffect(() => {
+        count.current++;
+        input1.current.focus();
+    },[value2])
+
     return (
         <div className="UseRef">
             <PageTitle
                 title="Hook UseRef"
                 subtitle="Retorna um objeto mutável com a propriedade .current!"
             />
+
+            <SectionTitle title="Exercício #01" />
+            <div className='center'>
+                <div>
+                    <span className="text">Valor: </span>
+                    <span className="text">{merge(value1, value2)}</span>
+                    <span className="text">[</span>
+                    <span className="text red">{count.current}</span>
+                    <span className="text">]</span>
+                </div>
+                <input 
+                    type="text" 
+                    className="input" 
+                    value={value1} 
+                    onChange={e => setValue1(e.target.value)}
+                    ref={input1}
+                />
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div>
+                <input 
+                    type="text" 
+                    className="input" 
+                    value={value2} 
+                    onChange={e => setValue2(e.target.value)}
+                    ref={input2}
+                />
+            </div>
         </div>
     )
 }
