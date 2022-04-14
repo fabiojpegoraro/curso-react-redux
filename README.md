6b5b17f0cf07f685533c979294af86c1285df93a
Fábio José Pegoraro <fabio.pegoraro@softplan.com.br>
2022-04-14 16:12:03 -0300

    finalização do todo-app sem redux

--- README.md
+++ README.md
@@ -3,7 +3,13 @@
 - Para o backend do projeto todo-app é necessário ter o mongo instalado e rodando.
 
 - Para executar o backend em modo dev:
-    npm run dev
+    Executar na raíz do projeto o comando npm run dev
 
 - Para executar o backend em modo produção (pm2):
-    npm run production
+    Executar na raíz do projeto o comando npm run production
+
+- Para executar o frontend em modo dev:
+    Executar na raíz do projeto o comando npm run dev
+
+- Para executar o backend em modo produção (webpack):
+    Executar na raíz do projeto o comando npm run production
--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -3,13 +3,44 @@ import Grid from '../template/grid';
 import Grid2 from '../template/grid2';
 import IconButton from '../template/iconButton';
 
-export default props => (
-    <div role='form' class='todoForm'>
-        <Grid2 cols='12 9 10'>
-            <input id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
-        </Grid2>
-        <Grid2 cols='12 3 2'>
-            <IconButton hide={false} style='primary' icon='plus' />
-        </Grid2>
-    </div>
-)
+export default props => {
+
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if(e.key ==='Escape'){
+            props.handleClearForm();
+        }
+    }
+
+    return(
+        <div role='form' className='todoForm'>
+            <Grid2 cols='12 9 10'>
+                <input id='description' 
+                    className='form-control' 
+                    placeholder='Adicione uma tarefa' 
+                    value={props.description}
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}>
+                </input>
+            </Grid2>
+            <Grid2 cols='12 3 2'>
+                <IconButton 
+                    hide={false} 
+                    style='primary' 
+                    icon='plus' 
+                    onClick={props.handleAdd}/>
+                <IconButton 
+                    hide={false} 
+                    style='info' 
+                    icon='search' 
+                    onClick={props.handleSearch}/>
+                            <IconButton 
+                    hide={false} 
+                    style='default' 
+                    icon='close' 
+                    onClick={props.handleClearForm}/>
+            </Grid2>
+        </div>
+    )
+}
