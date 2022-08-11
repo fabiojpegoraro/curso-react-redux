4cbeb1070947b57a36a4a2b676b2896757115b24
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-08-11 09:12:53 -0300

    Continuação do backend e primeiro teste rest

--- my-money-app/backend/src/api/billingCycle/billingCycleService.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleService.js
@@ -2,6 +2,7 @@ const BillingCycle = require('./billingCycle');
 
 BillingCycle.methods(['get', 'post', 'put', 'delete']);
 BillingCycle.updateOptions({ new: true, runValidators: true });
+
 BillingCycle.route('get', (req, res, next) => {
     BillingCycle.find({}, (err, docs) => {
         if (!err) {
