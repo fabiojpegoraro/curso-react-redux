f60fe9233437203da452c8ede6178ff99d3d9e97
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-09-07 10:57:06 -0300

    ajustes no frontend

--- my-money-app/frontend/webpack.config.js
+++ my-money-app/frontend/webpack.config.js
@@ -1,10 +1,10 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 module.exports = {
     entry: './src/index.jsx',
     output: {
-        path: __dirname + 'public',
+        path: __dirname + '/public',
         filename: './app.js'
     },
     devServer: {
@@ -14,7 +14,7 @@ module.exports = {
     resolve: {
         extensions: ['', '.js', '.jsx'],
         alias: {
-            modules: __dirname + 'node_modules',
+            modules: __dirname + '/node_modules',
             jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
             bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
         }
