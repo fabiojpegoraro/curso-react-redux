7d5b65395b6319395791a40b79791c7dd5234fa7
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-11-15 22:37:47 -0300

    Adição de formulários de créditos e débitos

--- my-money-app/frontend/src/billingCycle/billingCycleList.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycleList.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { getList } from './billingCycleActions';
+import { getList, showUpdate, showDelete } from './billingCycleActions';
 
 class BillingCycleList extends Component {
 
@@ -16,6 +16,10 @@ class BillingCycleList extends Component {
                 <td>{item.name}</td>
                 <td>{item.month}</td>
                 <td>{item.year}</td>
+                <td >
+                    <button className='btn btn-warning' onClick={() => this.props.showUpdate(item)}><i className='fa fa-pencil'></i></button>
+                    <button className='btn btn-danger' onClick={() => this.props.showDelete(item)}><i className='fa fa-trash-o'></i></button>
+                </td>
             </tr>
             ))
     }
@@ -29,6 +33,7 @@ class BillingCycleList extends Component {
                             <th>Nome</th>
                             <th>Mês</th>
                             <th>Ano</th>
+                            <th className="table-actions">Ações</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -41,5 +46,5 @@ class BillingCycleList extends Component {
 }
 
 const mapStateToProps = state => ({list: state.billingCycle.list});
-const mapDispatchToProps = dispatch => bindActionCreators({getList}, dispatch);
+const mapDispatchToProps = dispatch => bindActionCreators({getList, showUpdate, showDelete}, dispatch);
 export default connect(mapStateToProps, mapDispatchToProps)(BillingCycleList);
