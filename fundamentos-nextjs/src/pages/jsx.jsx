11e357543f1fe16cb4f066135da797fe66cd20ae
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2022-11-27 21:16:16 -0300

    Fundamentos NextJS

---
+++ fundamentos-nextjs/src/pages/jsx.jsx
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function Jsx() {
+    return (
+        <Layout titulo='Entendendo o JSX'>
+            <div>
+                <h1>JSX é um conceito central</h1>
+            </div>
+        </Layout>
+    )
+}
