f84db79f6c19e1cb507730dfb92005fde751d5db
Fábio José Pegoraro <fabiojpegoraro@gmail.com>
2023-01-25 18:49:52 -0300

    Finalização da implementação de autenticação

--- my-money-app/frontend/src/auth/authActions.js
+++ my-money-app/frontend/src/auth/authActions.js
@@ -9,3 +9,36 @@ export function login(values) {
 export function signup(values) {
     return submit(values, `${consts.OAPI_URL}/signup`);
 };
+
+function submit(values, url){
+    return dispatch => {
+        axios.post(url, values)
+            .then(resp => {
+                dispatch([
+                    {type: 'USER_FETCHED', payload: resp.data}
+                ])
+            })
+            .catch(e => {
+                e.response.data.errors.forEach(
+                    error => toastr.error('Error', error));
+            })
+    }
+};
+
+export function logout() {
+    return {type: 'TOKEN_VALIDATED', payload: false}
+};
+
+export function validateToken(token){
+    return dispatch => {
+        if(token){
+            axios.post(`${consts.OAPI_URL}/validateToken`, {token})
+                .then(resp => {
+                    dispatch({type: 'TOKEN_VALIDATED', payload: resp.data.valid});
+                })
+                .catch(e => dispatch({type: 'TOKEN_VALIDATED', payload: false}));
+        }else {
+            dispatch({type: 'TOKEN_VALIDATED', payload: false});
+        };
+    };
+};
